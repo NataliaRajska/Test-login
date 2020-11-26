39274d91672993863176436ebe73e26e781bed34
NataliaRajska <natalia.rajskaa@gmail.com>
2020-11-26 17:34:54 +0100

    Clean html from tutorial of Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClassicLoginComponent } from './classic-login/classic-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClassicLoginComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/classic-login/classic-login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-classic-login',
+  templateUrl: './classic-login.component.html',
+  styleUrls: ['./classic-login.component.css']
+})
+export class ClassicLoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
